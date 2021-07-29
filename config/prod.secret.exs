b1b4f86da6858acdf6f37308cc6e988454210956
Petros <petrosg@gmail.com>
2021-07-29 20:48:07 +0200

    fix syntax error: prod.secret.exs:17:3

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -13,7 +13,7 @@ database_url =
 
 config :pomodoro, Pomodoro.Repo,
   adapter: Ecto.Adapters.Postgres,
-  database: ""
+  database: "",
   ssl: true,
   url: database_url,
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
