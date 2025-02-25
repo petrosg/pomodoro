79e8734ed8a94fdfc24c2a70ce295a12f5d434ff
Petros Gkourasas <petrosg@gmail.com>
2025-02-25 09:10:02 +0200

    Update dependencies, adjust .gitignore, and modify configuration settings

--- mix.exs
+++ mix.exs
@@ -32,11 +32,11 @@ defmodule Pomodoro.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.7.14"},
-      {:phoenix_html, "~> 4.1"},
+      {:phoenix, "~> 1.7.19"},
+      {:phoenix_html, "~> 4.2"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       # TODO bump on release to {:phoenix_live_view, "~> 1.0.0"},
-      {:phoenix_live_view, "~> 1.0.0-rc.1", override: true},
+      {:phoenix_live_view, "~> 1.0.4"},
       {:floki, ">= 0.30.0", only: :test},
       {:phoenix_live_dashboard, "~> 0.8.3"},
       {:esbuild, "~> 0.8", runtime: Mix.env() == :dev},
